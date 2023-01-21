4068386110d0b318237bf8627946268b24958096
Maryna-Korbet <m22v86k@gmail.com>
2023-01-21 02:35:50 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import {Routes, Route} from "react-router-dom";
+import Home from "path/to/pages/Home";
+import About from "path/to/page/About";
+import Products  from "path/to/page/Products";
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+    return(
+        <div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/products" element={<Products/>} />
+            </Routes>
+        </div>
+    );
 };
