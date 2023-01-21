72d5d5284fa8534cf1f5e56ecab910dd5e342e6e
Maryna-Korbet <m22v86k@gmail.com>
2023-01-21 03:38:17 +0200

    added Link and NavLink

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,33 @@ import {Routes, Route} from "react-router-dom";
 import {Home} from "pages/Home";
 import {About} from "pages/About";
 import {Products}  from "pages/Products";
-import {NotFound} from "pages/NotFound";
+// import {NotFound} from "path/to/pages/NotFound";
+import { Container, Header, Logo, Link } from "./App.styled";
 
 export const App = () => {
-    return(
-        <div>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/products" element={<Products/>} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-        </div>
+    return (
+      <Container>
+        <Header>
+          <Logo>
+            <span role="img" aria-label="computer icon">
+              💻
+            </span>{" "}
+            GoMerch Store
+          </Logo>
+          <nav>
+            <Link to="/" end>
+              Home
+            </Link>
+            <Link to="/about">About</Link>
+            <Link to="/products">Products</Link>
+          </nav>
+        </Header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          {/* <Route path="*" element={<NotFound />} /> */}
+        </Routes>
+      </Container>
     );
-};
+  };
