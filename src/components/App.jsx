5c2121f74980527644743498879b8b3a335b3b84
Maryna-Korbet <m22v86k@gmail.com>
2023-01-21 02:36:57 +0200

    added navigation error page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import {Routes, Route} from "react-router-dom";
 import Home from "path/to/pages/Home";
 import About from "path/to/page/About";
 import Products  from "path/to/page/Products";
+import NotFound from "path/to/page/NotFound";
 
 export const App = () => {
     return(
@@ -10,6 +11,7 @@ export const App = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/products" element={<Products/>} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </div>
     );
