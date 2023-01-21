c66c6c81d3aff2cd2e17a7a948a02d3b6637f0c7
Maryna-Korbet <m22v86k@gmail.com>
2023-01-21 03:59:53 +0200

    added nested routes Mission, Reviews, Team

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,9 @@ import {About} from "pages/About";
 import {Products}  from "pages/Products";
 // import {NotFound} from "path/to/pages/NotFound";
 import { ProductDetails } from "pages/ProductDetails";
+import { Mission } from "./Mission";
+import { Reviews } from "./Reviews";
+import { Team } from "./Team";
 import { Container, Header, Logo, Link } from "./App.styled";
 
 export const App = () => {
@@ -26,7 +29,11 @@ export const App = () => {
         </Header>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/about" element={<About />}>
+                <Route path="mission" element={<Mission />} />
+                <Route path="team" element={<Team />} />
+                <Route path="reviews" element={<Reviews />} />
+          </Route>
           <Route path="/products" element={<Products />} />
           <Route path="/products/:id" element={<ProductDetails/>} />
           {/* <Route path="*" element={<NotFound />} /> */}
