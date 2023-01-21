d8036b5384944642e63fc8b30950f87c4cd5f6f0
Maryna-Korbet <m22v86k@gmail.com>
2023-01-21 03:03:08 +0200

    updated App, styles, installed npm i modern-normalize

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import {Routes, Route} from "react-router-dom";
-import Home from "path/to/pages/Home";
-import About from "path/to/page/About";
-import Products  from "path/to/page/Products";
-import NotFound from "path/to/page/NotFound";
+import {Home} from "pages/Home";
+import {About} from "pages/About";
+import {Products}  from "pages/Products";
+import {NotFound} from "pages/NotFound";
 
 export const App = () => {
     return(
